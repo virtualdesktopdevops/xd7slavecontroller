e530375c02707fd0dfee7b8db082e3b56e1354f6
matt6697 <32440697+matt6697@users.noreply.github.com>
2017-12-11 08:22:00 +0100

    Trust requests sent to XML service
    Trust requests sent to XML service. Required for domain-passthrough or smartcard logon

--- manifests/joinsite.pp
+++ manifests/joinsite.pp
@@ -10,6 +10,20 @@ class xd7slavecontroller::joinsite inherits xd7slavecontroller {
 	  dsc_sitename => $sitename,
 	  dsc_existingcontrollername => $site_mastercontroller,
 	  dsc_credential => {'user' => $svc_username, 'password' => $svc_password} 
-	}
+	}->
+	
+  #Trust request sent to XML service
+  dsc_script{ 'CitrixBrokerServiceSSL':
+    dsc_getscript => 'Add-PSSnapin Citrix*
+     Return @{ Result = [bool]$(Get-BrokerSite | fl TrustRequestsSentToTheXmlServicePort) }',
+    dsc_testscript => 'Add-PSSnapin Citrix*
+     If (Get-BrokerSite | fl TrustRequestsSentToTheXmlServicePort) {
+       Return $true
+     } Else {
+       Return $false
+     }',
+    dsc_setscript => 'Add-PSSnapin Citrix*
+     Set-BrokerSite -TrustRequestsSentToTheXmlServicePort $true'
+  }
 	
 }
