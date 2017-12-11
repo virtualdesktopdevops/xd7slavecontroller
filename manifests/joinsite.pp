782dfb600f4fb12f868dec840d9f4e24ad090922
matt6697 <32440697+matt6697@users.noreply.github.com>
2017-12-11 18:30:33 +0100

    Trust requests sent to Citrix XML Service
    Trust requests sent to Citrix XML Service

--- manifests/joinsite.pp
+++ manifests/joinsite.pp
@@ -10,6 +10,23 @@ class xd7slavecontroller::joinsite inherits xd7slavecontroller {
 	  dsc_sitename => $sitename,
 	  dsc_existingcontrollername => $site_mastercontroller,
 	  dsc_credential => {'user' => $svc_username, 'password' => $svc_password} 
-	}
+	}->
+	
+  #Trust request sent to XML service
+  dsc_script{ 'TrustRequestsToXMLService':
+    dsc_getscript => 'Add-PSSnapin -Name Citrix.Broker.Admin.V2 -ErrorAction SilentlyContinue
+		$brokersite = Get-BrokerSite
+	   Return @{ Result = $brokersite.TrustRequestsSentToTheXmlServicePort) }',
+    dsc_testscript => 'Add-PSSnapin -Name Citrix.Broker.Admin.V2 -ErrorAction SilentlyContinue
+	   $brokersite = Get-BrokerSite
+	   If ($brokersite.TrustRequestsSentToTheXmlServicePort) {
+	     Return $true
+	   } Else {
+       Return $false
+	   }',
+    dsc_setscript => 'Add-PSSnapin -Name Citrix.Broker.Admin.V2 -ErrorAction SilentlyContinue
+	   Set-BrokerSite -TrustRequestsSentToTheXmlServicePort $true',
+	dsc_psdscrunascredential => {'user' => $svc_username, 'password' => $svc_password}
+  }
 	
 }
